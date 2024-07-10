489952e73bebb126d34c45f2da4338688eeeac3a
Jaydeep Damani <jaydeep.damani@192.168.1.7>
2024-07-10 18:50:39 +0530

    code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { ThemeProvider } from "@mui/material";
 import React from "react";
-import { useSelector } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 import { logger, MUITheme } from "./helpers";
 
 function App() {
-  const state = useSelector((s: any) => s.alert);
   logger.info("Env: ", process.env.REACT_APP_ENV);
 
   return (
